dee1d0108c7aa83b85d37285c9ea4bae21ad78e7
Crypto Pixel <lebui2892@gmail.com>
2025-10-25 16:28:31 -0700

    First rust program: Convert to string

---
+++ weekly/problems/rust/converter.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // A temperature in Celsius
+    let temp_celsius: f64 = 23.75;
+    
+    // TODO: Convert the temperature from a f64 to a String
+    let temperature_text: String = temp_celsius.to_string();
+    
+    // Displaying the temperature as text
+    println!("The temperature is: {}°C", temperature_text);
+}
